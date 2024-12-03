0b7d28950e26bb02a5ac99ec89c729ed81eb11b3
Anaiis98 <anaismarissal1998@gmail.com>
2024-12-03 14:35:51 +0100

    modification visual.py

--- visual.py
+++ visual.py
@@ -1,8 +1,15 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import os
 
-data = pd.read_csv("output_data.csv")
-plt.plot(data['x'], data['y'])
-plt.title("Example Visualization")
-plt.savefig("visualization.png")
+print("Current working directory:", os.getcwd())
+try:
+    data = pd.read_csv("output_data.csv")
+    print("Data loaded successfully:\n", data.head())
+    plt.plot(data['x'], data['y'])
+    plt.title("Example Visualization")
+    plt.savefig("visualization.png")
+    print("Image saved successfully as 'visualization.png'")
+except Exception as e:
+    print("An error occurred:", str(e))
 
